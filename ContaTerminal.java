5112687b02ffd9325f4e9c4903ed7b9c0a033ce4
Dimitri <dimitriiff00@gmail.com>
2025-03-26 19:44:27 -0300

    Criando a interação com o Usuario através da classe Scanner e definindo diferentes tipos de variáveis

--- ContaTerminal.java
+++ ContaTerminal.java
@@ -1,2 +1,33 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
+    public static void main(String[] args) {
+        //TODO: Conhecer e importar a classe Scanner
+        Scanner scanner = new Scanner(System.in);
+        Locale.setDefault(Locale.US);
+
+        //Exibir as mensagens para o usuario
+        //obter pelo scanner os valores digitados no terminal
+
+        System.out.println("Digite o numero da conta: ");
+        int conta = scanner.nextInt();
+
+        scanner.nextLine();
+
+        System.out.println("Digite a Agencia: ");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Digite o nome do Titular: ");
+        String titular = scanner.nextLine();
+
+        System.out.println("Digite o saldo em conta: ");
+        double saldo = scanner.nextDouble();
+
+        //exibir as mensagens de conta criada
+        System.out.println("Olá " + titular+"," + " obrigado por criar uma conta em nosso banco, sua agência é: " + agencia +"," + " conta: " + conta + " e seu saldo de " + saldo + " já está disponível para saque");
+
+
+    }
+
 }
